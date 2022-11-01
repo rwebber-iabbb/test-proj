539b932adf5fcee141aebb0edd5c313f3120efef
Robert Webber <rwebber@iabbb.org>
2022-11-01 13:55:20 -0500

    another try

--- scripts/AfterInstallHook.sh
+++ scripts/AfterInstallHook.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-source /home/ec2-user/.bashrc
+export NVM_DIR="/home/ec2-user/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 cd /var/www/html
 nvm install 16
 npm install
--- scripts/BeforeInstallHook.sh
+++ scripts/BeforeInstallHook.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-source /home/ec2-user/.bashrc
+source /home/ec2-user/.bash_profile
 yum update -y
 # pm2 update
