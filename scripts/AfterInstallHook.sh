b96c70985bb0a8d22f0c2638136df7d64eddac1c
Robert Webber <rwebber@iabbb.org>
2022-11-01 14:40:06 -0500

    wip

--- scripts/AfterInstallHook.sh
+++ scripts/AfterInstallHook.sh
@@ -6,6 +6,7 @@ echo "setting nvm path"
 export NVM_DIR="/home/ec2-user/.nvm"
 echo "setting second part of nvm path"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+echo "running delete-prefix"
 nvm use --delete-prefix v19.0.0 --silent
 echo "changing Directory"
 cd /var/www/html
