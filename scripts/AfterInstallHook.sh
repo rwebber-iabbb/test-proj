d7c7f631fab6aeeeebe5ebad94dcc5b013b88260
Robert Webber <rwebber@iabbb.org>
2022-11-01 14:13:07 -0500

    doop

--- scripts/AfterInstallHook.sh
+++ scripts/AfterInstallHook.sh
@@ -2,7 +2,11 @@
 set -e
 export NVM_DIR="/home/ec2-user/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+echo "changing Directory"
 cd /var/www/html
+echo "changing Directory"
 nvm install 16
+echo "changing Directory"
 npm install
+echo "running build"
 npm run build
