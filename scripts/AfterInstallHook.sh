ed285ab55da1be846a26c97fba1373f15442f15b
Robert Webber <rwebber@iabbb.org>
2022-11-01 12:49:35 -0500

    load nvm maybe

--- scripts/AfterInstallHook.sh
+++ scripts/AfterInstallHook.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
-cd /usr/share/nginx/html
+cd /var/www/html
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install 16
 npm install
 npm run build
